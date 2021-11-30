a003dd7b54c3f29e0eff0db2a34c0c8b47be361c
bost-ty <61333914+bost-ty@users.noreply.github.com>
2021-11-30 15:25:15 -0800

    Create /twitch route

---
+++ twitch/twitch.js
@@ -0,0 +1,7 @@
+/* Following some information from: 
+*    https://dev.twitch.tv/docs/authentication/getting-tokens-oauth#oauth-implicit-code-flow
+*    https://stackoverflow.com/questions/61647395/how-to-add-oauth-token-in-js-for-twitch-api
+*    https://discuss.dev.twitch.tv/t/requiring-oauth-for-helix-twitch-api-endpoints/23916
+*    https://github.com/twitchdev/authentication-node-sample/blob/main/index.js
+*    https://discuss.dev.twitch.tv/t/getting-user-ids/13806/8
+*/
