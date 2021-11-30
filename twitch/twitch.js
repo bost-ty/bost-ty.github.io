68860fa5dbbf548e2f6dac0db0742f6665a22b21
bost-ty <61333914+bost-ty@users.noreply.github.com>
2021-11-30 15:33:30 -0800

    Install Prettier

--- twitch/twitch.js
+++ twitch/twitch.js
@@ -1,15 +1,15 @@
-/* Following some information from: 
-*    https://dev.twitch.tv/docs/authentication/getting-tokens-oauth#oauth-implicit-code-flow
-*    https://stackoverflow.com/questions/61647395/how-to-add-oauth-token-in-js-for-twitch-api
-*    https://discuss.dev.twitch.tv/t/requiring-oauth-for-helix-twitch-api-endpoints/23916
-*    https://github.com/twitchdev/authentication-node-sample/blob/main/index.js
-*    https://discuss.dev.twitch.tv/t/getting-user-ids/13806/8
-*/
+/* Following some information from:
+ *    https://dev.twitch.tv/docs/authentication/getting-tokens-oauth#oauth-implicit-code-flow
+ *    https://stackoverflow.com/questions/61647395/how-to-add-oauth-token-in-js-for-twitch-api
+ *    https://discuss.dev.twitch.tv/t/requiring-oauth-for-helix-twitch-api-endpoints/23916
+ *    https://github.com/twitchdev/authentication-node-sample/blob/main/index.js
+ *    https://discuss.dev.twitch.tv/t/getting-user-ids/13806/8
+ */
 
-console.info("Page loaded")
+console.info("Page loaded");
 
-const authBtn = document.getElementById("authBtn")
+const authBtn = document.getElementById("authBtn");
 
-document.addEventListener("click", authBtn, e => {
-    console.log(e)
-})
+document.addEventListener("click", authBtn, (e) => {
+  console.log(e);
+});
