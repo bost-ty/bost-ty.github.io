0ceb48cd4984898d985eb0d1daee63a0f66fb28f
bost-ty <61333914+bost-ty@users.noreply.github.com>
2021-12-01 10:55:47 -0800

    Reorganize comments

--- twitch/twitch.js
+++ twitch/twitch.js
@@ -24,31 +24,6 @@ async function onAuthSubmit() {
   return await fetch(URL, {});
 }
 
-/** Example POST method implementation:
- *   async function postData(url = '', data = {}) {
- *     // Default options are marked with *
- *     const response = await fetch(url, {
- *       method: 'POST', // *GET, POST, PUT, DELETE, etc.
- *       mode: 'cors', // no-cors, *cors, same-origin
- *       cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
- *       credentials: 'same-origin', // include, *same-origin, omit
- *       headers: {
- *         'Content-Type': 'application/json'
- *         // 'Content-Type': 'application/x-www-form-urlencoded',
- *       },
- *       redirect: 'follow', // manual, *follow, error
- *       referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
- *       body: JSON.stringify(data) // body data type must match "Content-Type" header
- *     });
- *     return response.json(); // parses JSON response into native JavaScript objects
- *   }
- *
- *   postData('https://example.com/answer', { answer: 42 })
- *     .then(data => {
- *       console.log(data); // JSON data parsed by `data.json()` call
- *     });
- **/
-
 // Define variables for ease of use, clarity of future requests
 const baseURL = "https://api.twitch.tv/helix";
 const twitchAuthPOSTUrl = `${baseURL}/some/path`;
@@ -78,6 +53,32 @@ async function fetchAsync(url) {
 
 // "Get Twitch Channel"
 async function getTwitchChannel(user) {
-  token = "";
   return;
 }
+
+/* ----------
+
+// Example POST method implementation:
+async function postData(url = "", data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+postData("https://example.com/answer", { answer: 42 }).then((data) => {
+  console.log(data); // JSON data parsed by `data.json()` call
+});
+
+---------- */
