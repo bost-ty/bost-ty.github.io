d1276a508567779fb0a50f9d2a952b22095f6741
bost-ty <61333914+bost-ty@users.noreply.github.com>
2021-12-02 17:39:16 -0800

    Fine tuning

--- twitch/twitch.js
+++ twitch/twitch.js
@@ -129,7 +129,7 @@ async function fetchUserInformation(username) {
     .then((res) => res.json())
     .catch((err) => console.log("Error: " + err));
 
-  console.log(userInformation.data);
+  console.log(userInformation.data[0]);
   return userInformation;
 }
 
